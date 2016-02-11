973fd6cdced79a96decd2702c32a052d77d45662
Nolan Frazier <nofrazier3@gmail.com>
2016-02-11 13:49:00 -0800

    rspec for numerology app

--- 204/numerology_app/index.rb
+++ 204/numerology_app/index.rb
@@ -20,15 +20,15 @@ def get_birth_path_num(birthdate)
   end
 
   numerology = {
-  	1 => "Your numerology number is #{sum}.\nOne is the leader. The number one indicates the ability to stand alone, and is a stong vibration. Ruled by the Sun.",
+  	1 => "Your numerology number is #{sum}.\nOne is the leader. The number one indicates the ability to stand alone, and is a strong vibration. Ruled by the Sun.",
   	2 => "Your numerology number is #{sum}.\nThis is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon.",
-  	3 => "Your numerology number is #{sum}.\nNumber three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three’s enjoy life and have a good sense of humor. Ruled by Jupiter.",
-  	4 => "Your numerology number is #{sum}.\nThis is the worker. Practical, with a love of detail, fours are trustworthy, hard-working, and helpful. Ruled by Uranus.",
-  	5 => "Your numerology number is #{sum}.\nThis is the freedom lover. The number five is an intellectual vibration. These are ‘idea’ people with a love of variety and the ability to adapt to most situations. Ruled by Mercury.",
+  	3 => "Your numerology number is #{sum}.\nNumber Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three's enjoy life and have a good sense of humor. Ruled by Jupiter.",
+  	4 => "Your numerology number is #{sum}.\nThis is the worker. Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful. Ruled by Uranus.",
+  	5 => "Your numerology number is #{sum}.\nThis is the freedom lover. The number five is an intellectual vibration. These are 'idea' people with a love of variety and the ability to adapt to most situations. Ruled by Mercury.",
   	6 => "Your numerology number is #{sum}.\nThis is the peace lover. The number six is a loving, stable, and harmonious vibration. Ruled by Venus.",
   	7 => "Your numerology number is #{sum}.\nThis is the deep thinker. The number seven is a spiritual vibration. These people are not very attached to material things, are introspective, and generally quiet. Ruled by Neptune.",
-  	8 => "Your numerology number is #{sum}.\nThis is the manager. Number eight is a strong, successful, and material vibration. Ruled by Saturn.",
-  	9 => "Your numerology number is #{sum}.\nThis is the teacher. Number nine is a tolerant, somewhat impractical, and sympathetic vibration. Ruled by Mars."
+  	8 => "Your numerology number is #{sum}.\nThis is the manager. Number Eight is a strong, successful, and material vibration. Ruled by Saturn.",
+  	9 => "Your numerology number is #{sum}.\nThis is the teacher. Number Nine is a tolerant, somewhat impractical, and sympathetic vibration. Ruled by Mars."
   }
 
   $message = numerology[sum]
